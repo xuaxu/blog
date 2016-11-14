234de96edc845cc8a2d44e95ca5daad4f229a35b
Chen Liang <chenliang@3tcloud.com>
2016-11-14 19:20:06 +0800

    add login

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from . import db
+from . import login_manager
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -16,8 +18,10 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(64), unique = True, index = True)
     username = db.Column(db.String(64), unique = True, index = True)
     password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -34,5 +38,6 @@ class User(db.Model):
         return check_password_hash(self.password_hash, password)
 
 
-if __name__ == '__main__':
-    db.create_all()
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
